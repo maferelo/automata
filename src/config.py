1927069033c8c4124e4a18771b57edb7f2a7cf9f
Felipe Restrepo <maferelo@users.noreply.github.com>
2022-06-06 19:19:05 -0500

    Config

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+SPOTIFY_USERNAME = os.environ.get('SPOTIFY_USERNAME') or ''
+DOWNLOADS_DIRECTORY = os.environ.get('DOWNLOADS_DIRECTORY') or ''
