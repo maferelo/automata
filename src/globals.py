6f85bb086c0865fc8df9dbf2cdce988f50c4ee45
Felipe Restrepo <maferelo@users.noreply.github.com>
2022-06-06 19:17:05 -0500

    Globals

---
+++ src/globals.py
@@ -0,0 +1,7 @@
+import spotipy
+import logs
+
+auth_manager = spotipy.oauth2.SpotifyClientCredentials()
+spotipy_session = spotipy.Spotify(auth_manager=auth_manager)
+
+logger = logs.create_logger()
