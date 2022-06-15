303d84b55cecae72506377b7b38860691404ac5a
maferelo <maferelo13@gmail.com>
2022-06-15 15:41:41 -0500

    Basic crontab

--- src/apps/shceluder.py
+++ src/apps/shceluder.py
@@ -6,7 +6,4 @@ def set_tasks() -> None:
     """Set the cronjobs."""
     with CronTab(user="root") as cron:
         job = cron.new(command="echo hello_world")
-        # cron = Crontab()
-        # cron.add("* * * * *", "python3 /home/pi/src/app.py")
-        # cron.write()
         job.minute.every(1)
