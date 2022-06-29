840449beb3ee06ba954f679ed51383c030c449cd
maferelo <maferelo13@gmail.com>
2022-06-29 07:30:24 -0500

    Basic flaskapp with postgres

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
-"""."""
 from fastapi import FastAPI
 
-app = FastAPI(title="FastAPI, Docker and Traefik")
+from app.db import User, database
+
+app = FastAPI(title="FastAPI, Docker, and Traefik")
 
 
 @app.get("/")
-def read_root():
-    """."""
-    return {"hello": "world....."}
+async def read_root():
+    return await User.objects.all()
+
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await User.objects.get_or_create(email="test@test.com")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
