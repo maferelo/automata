ab68595e52ec39a2eb71cdea29f1a58b40dcbc5d
maferelo <maferelo13@gmail.com>
2022-06-29 19:22:15 -0500

    Db querys on

--- app/main.py
+++ app/main.py
@@ -7,17 +7,16 @@ app = FastAPI(title="FastAPI, Docker, and Traefik")
 
 @app.get("/")
 async def read_root():
-    # return await User.objects.all()
-    return [{"id": 1, "email": "test@test.com", "active": True}]
+    return await User.objects.all()
+    # return [{"id": 1, "email": "test@test.com", "active": True}]
 
 
 @app.on_event("startup")
 async def startup():
-    # if not database.is_connected:
-    #    await database.connect()
+    if not database.is_connected:
+        await database.connect()
     # create a dummy entry
-    # await User.objects.get_or_create(email="test@test.com")
-    pass
+    await User.objects.get_or_create(email="test@test.com")
 
 
 @app.on_event("shutdown")
