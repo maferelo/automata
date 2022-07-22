52627c53ecbe63d34f47fc53f53b16f30299ed32
maferelo <maferelo13@gmail.com>
2022-07-22 18:43:30 -0500

    Telegram bot env variables

--- app/config.py
+++ app/config.py
@@ -7,7 +7,10 @@ class Settings(BaseSettings):
     db_url: str = Field(..., env="DATABASE_URL")
     db_engine_pool_size: int = Field(20, env="DATABASE_ENGINE_POOL_SIZE")
     db_engine_max_overflow: int = Field(0, env="DATABASE_ENGINE_MAX_OVERFLOW")
+    telegram_chat_id: str = Field(..., env="TELEGRAM_CHAT_ID")
+    telegram_token: str = Field(..., env="TELEGRAM_TOKEN")
 
 
 settings = Settings()
+# Required for postgres Heroku deprecated postgres naming
 settings.db_url = settings.db_url.replace("postgres://", "postgresql://", 1)
