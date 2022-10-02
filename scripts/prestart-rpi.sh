15ec11dded01ef8f1a513f21c70dca582c2d7308
maferelo <maferelo13@gmail.com>
2022-10-02 18:21:59 -0500

    pyenv venv more general

--- scripts/prestart-rpi.sh
+++ scripts/prestart-rpi.sh
@@ -19,9 +19,7 @@ sh get-docker.sh
 
 cp .env.example .env
 
-docker-compose -f docker-compose.rpi.yml build
-
-docker-compose -f docker-compose.rpi.yml up
+sudo docker compose -f docker-compose.rpi.yml up
 
 curl https://pyenv.run | bash
 export PYENV_ROOT="$HOME/.pyenv"
@@ -29,9 +27,9 @@ command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 pyenv install 3.8.13
-pyenv virtualenv automata
-pyenv global automata
-pyenv activate automata
+pyenv virtualenv venv
+pyenv global venv
+pyenv activate venv
 echo "export PYENV_ROOT=$HOME/.pyenv" >> ~/.bashrc
 echo "command -v pyenv >/dev/null || export PATH=$PYENV_ROOT/bin:$PATH" >> ~/.bashrc
 echo "eval $(pyenv init -)" >> ~/.bashrc
@@ -39,6 +37,7 @@ echo "eval $(pyenv virtualenv-init -)" >> ~/.bashrc
 
 curl -sSL https://install.python-poetry.org | python3 -
 export PATH="$HOME/.local/bin:$PATH"
+echo "export PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
 
 poetry config virtualenvs.create false
 poetry install
